d820db9455f4d53c2b0828181fd4e00e7d7bc163
remcv <remuluscatana@tutanota.com>
2020-04-04 15:27:57 +0300

    Deleted unnecessary code again

--- app/src/main/java/com/example/todayweather/MainActivity.java
+++ app/src/main/java/com/example/todayweather/MainActivity.java
@@ -108,8 +108,8 @@ public class MainActivity extends AppCompatActivity
                     if (wObs != null)
                     {
                         database.add(wObs);
+                        mListener.handleEndOfNetworking();
                     }
-                    mListener.handleEndOfNetworking();
                 }
             });
         }
